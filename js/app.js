f7b5b5e3612d44641f260f47fee99b29f42f2eb0
Wanjugu Irene <ireneemaina@gmail.com>
2016-09-05 21:29:50 +0300

    Gallery Page Created

--- js/app.js
+++ js/app.js
@@ -8,14 +8,14 @@ $(document).ready(function() {
 });
 
 	// Initialize Masonry
-//	$('#content').masonry({
-//		columnWidth: 320,
-//		itemSelector: '.item',
-//		isFitWidth: true,
-//		isAnimated: !Modernizr.csstransitions
-//	}).imagesLoaded(function() {
-//		$(this).masonry('reload');
-//	});
+	$('#content').masonry({
+		columnWidth: 320,
+		itemSelector: '.item',
+		isFitWidth: true,
+		isAnimated: !Modernizr.csstransitions
+	}).imagesLoaded(function() {
+		$(this).masonry('reload');
+	});
 
 });
 
